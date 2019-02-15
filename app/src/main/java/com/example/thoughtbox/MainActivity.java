92630f1f501e36a4bb415d0ba5dffa04003f9531
eChadwick <en.chadwick@gmail.com>
2019-02-14 22:53:19 -0600

    add new thought activity

--- app/src/main/java/com/example/thoughtbox/MainActivity.java
+++ app/src/main/java/com/example/thoughtbox/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.thoughtbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSaveThought(View view) {
+        TextView thoughtBox = findViewById(R.id.NewThoughtBox);
+        String thought = thoughtBox.getText().toString();
+        int puppies = 4;
     }
 }
