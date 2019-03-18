ecbc58fb1ead412599469619466c2d031eaf88ea
eChadwick <en.chadwick@gmail.com>
2019-03-18 10:29:55 -0500

    remove unnecessary crap

--- app/src/main/java/com/example/thoughtbox/CreateThought.java
+++ app/src/main/java/com/example/thoughtbox/CreateThought.java
@@ -1,6 +1,5 @@
 package com.example.thoughtbox;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +7,22 @@ import android.widget.TextView;
 
 public class CreateThought extends AppCompatActivity {
 
-    ThoughtViewModel mThoughtViewModel;
     ThoughtRoomDatabase mDb;
+    ThoughtDao mThoughtDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_thought);
-        mThoughtViewModel = ViewModelProviders.of(this).get(ThoughtViewModel.class);
+        mDb = ThoughtRoomDatabase.getDatabase(this);
+        mThoughtDao = mDb.thoughtDao();
     }
 
     public void onSaveThought(View view) {
         TextView thoughtBox = findViewById(R.id.NewThoughtBox);
-        String thought = thoughtBox.getText().toString();
-        mThoughtViewModel.insert(new Thought(thought));
+        String theThought = thoughtBox.getText().toString();
+        mThoughtDao.insert(new Thought(theThought));
+//        mThoughtViewModel.insert(new Thought(thought));
+
     }
 }
--- app/src/main/java/com/example/thoughtbox/MainActivity.java
+++ app/src/main/java/com/example/thoughtbox/MainActivity.java
@@ -1,20 +1,17 @@
 package com.example.thoughtbox;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ThoughtViewModel mThoughtViewModel;
+    Class LaunchActivity = ShowAllThoughts.class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent startUpIntent = new Intent(this, ShowAllThoughts.class);
+        Intent startUpIntent = new Intent(this, LaunchActivity);
         startActivity(startUpIntent);
     }
     
