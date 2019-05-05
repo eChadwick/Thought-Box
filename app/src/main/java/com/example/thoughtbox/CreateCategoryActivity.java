f1bb581f29ec7c03788a26c0fb46dd009e02a84f
eChadwick <en.chadwick@gmail.com>
2019-05-05 16:02:37 -0500

    member thoughts list on edit category view

--- app/src/main/java/com/example/thoughtbox/CreateCategoryActivity.java
+++ app/src/main/java/com/example/thoughtbox/CreateCategoryActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateCategoryActivity extends AppCompatActivity {
 
     ThoughtBoxRoomDatabase mDb;
     CategoryDao mCategoryDao;
+    CategoryThoughtDao mCategoryThoughtDao;
+    ThoughtDao mThoughtDao;
     Intent mCallingIntent;
 
     @Override
@@ -20,19 +26,33 @@ public class CreateCategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_category);
 
         Toolbar theToolbar = findViewById(R.id.create_category_toolbar);
-        theToolbar.setTitle("Add a Category");
 
         mDb = ThoughtBoxRoomDatabase.getDatabase(this);
         mCategoryDao = mDb.categoryDao();
+        mCategoryThoughtDao = mDb.categoryThoughtDao();
+        mThoughtDao = mDb.thoughtDao();
         mCallingIntent = getIntent();
 
         int callingId = mCallingIntent.getIntExtra( "CategoryId", -1);
         if(-1 != callingId) {
+            theToolbar.setTitle("Edit Category");
             String categoryName = mCategoryDao.getCategory(callingId).getName();
             TextView theView = findViewById(R.id.category_name_field);
             theView.setText(categoryName);
+
+            List<CategoryThought> categoryThoughts = mCategoryThoughtDao.getByCategoryId(callingId);
+            List<Thought> memberThoughts = new ArrayList<Thought>();
+
+            for(int i = 0; i < categoryThoughts.size(); i++){
+                memberThoughts.add(mThoughtDao.getThought(categoryThoughts.get(i).getThoughtId()));
+            }
+            ListView theList = findViewById(R.id.member_thoughts_list);
+            SelectThoughtListAdapter adapter = new SelectThoughtListAdapter(this, memberThoughts);
+
+            theList.setAdapter(adapter);
         }
         else {
+            theToolbar.setTitle("Add a Category");
             Button addThoughtsButton = (Button) findViewById(R.id.add_thoughts_to_category_button);
             addThoughtsButton.setVisibility(View.GONE);
         }
